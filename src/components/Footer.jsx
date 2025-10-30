72214ee0127c51b31fb5785f8b927436b8e8e90a
Pubudu Wijesundara <neo17641@gmail.com>
2025-10-30 10:14:46 +0530

    fix: modify text size in footer in mobile view

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="w-full border-t border-border font-medium text-sm text-muted-foreground transition-colors duration-300">
+    <footer className="w-full border-t border-border font-medium text-xs md:text-sm text-muted-foreground transition-colors duration-300">
       <div className="w-full px-8 sm:px-16 lg:px-32 py-2 flex items-center justify-center bg-background/80 backdrop-blur-sm">
         <span>
           &copy; {new Date().getFullYear()} Pubudu Wijesundara. All Rights
